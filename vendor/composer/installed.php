0c7cbf4bf4de3dd4770a354c735884988beb33b9
LuizHenriqueMP <98365070+LuizHenriqueMP@users.noreply.github.com>
2025-08-07 16:34:03 -0300

    Update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'luiz.pacheco/lista-de-contatos',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '239bc3f4c43f78cffe1e807e59c0a64278aa6743',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'luiz.pacheco/lista-de-contatos' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '239bc3f4c43f78cffe1e807e59c0a64278aa6743',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
